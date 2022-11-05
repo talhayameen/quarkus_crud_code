414cd6e81c09fb4464b9e6c4282c65ef3bdee14f
Talha Yameen <talhasam48@gmail.com>
2022-11-05 07:39:31 +0500

    msg

--- src/main/java/com/teresol/revision/dto/StudentDto.java
+++ src/main/java/com/teresol/revision/dto/StudentDto.java
@@ -17,7 +17,7 @@ public class StudentDto{
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
-    public String getUser_name( ) {
+    public String getUser_name() {
         return user_name;
     }
     public void setUser_name(String user_name) {
